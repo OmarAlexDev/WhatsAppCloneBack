c8936d5cefec6df827a3aecc6e93b9b41e04062e
OmarAlexDev <69879119+OmarAlexDev@users.noreply.github.com>
2023-01-12 22:02:59 -0600

    production command added for deployment

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const NODE_ENV = process.env.NODE_ENV
-const MONGO_URL = NODE_ENV=="DEV" ? process.env.MONGO_URL : process.env.MONGO_URL_TEST
+const MONGO_URL = NODE_ENV==="test" ? process.env.MONGO_URL_TEST : process.env.MONGO_URL 
 const SECRET = process.env.SECRET
 const PRIVATE = process.env.PRIVATE
 
