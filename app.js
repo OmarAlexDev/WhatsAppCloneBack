739319ff469bbf6aa7eea0a7c5c17c5ff6a35325
OmarAlexDev <69879119+OmarAlexDev@users.noreply.github.com>
2023-01-12 21:45:54 -0600

    dist folder added and served from server

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ mongoose.connection.on('error',(err)=>{
 })
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(`/${config.PRIVATE}/api/login`,loginRouter)
